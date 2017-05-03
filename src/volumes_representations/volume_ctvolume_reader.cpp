ff546d3938701994e17099e4d16b9204526dec7c
PabloBotas <kw375@ix.urz.uni-heidelberg.de>
2017-05-02 20:47:37 -0400

    Check stream status

--- src/volumes_representations/volume_ctvolume_reader.cpp
+++ src/volumes_representations/volume_ctvolume_reader.cpp
@@ -18,9 +18,15 @@ void Ctvolume_reader_t::read_file(std::string file)
 
     //  open file
     std::ifstream stream(file, std::ios::binary | std::ios::ate);
-    nElements = stream.tellg()*sizeof(short);
+    if (!stream.is_open())
+    {
+    	std::cerr << "ERROR! Planning CT \"" << file << "\" could not be opened." << std::endl;
+    	exit(EXIT_FAILURE);
+    }
+
+    nElements = stream.tellg()/sizeof(short);
+    size_t bytes_to_read = nElements*sizeof(short);
     stream.seekg(0, std::ios::beg);
-    unsigned int bytes_to_read = nElements*sizeof(short);
     hu.resize(nElements);
     stream.read(reinterpret_cast<char*>(&hu[0]), bytes_to_read);
 }
