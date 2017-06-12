0c9e86d9d7840b7524b9dfdb72b5a922a2c2e413
PabloBotas <kw375@ix.urz.uni-heidelberg.de>
2017-06-12 16:07:37 -0400

    Added simple test file and initial report

--- test/test_simple.sh
+++ test/test_simple.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-../adaptSpotEnergies --patient P15_base_plan \
+../adaptSpotEnergies --patient P15_base_plan_simple \
                      --cbct adapt_data_P15/week6_cbct.mha \
                      --vf adapt_data_P15/week6_xform.mha
