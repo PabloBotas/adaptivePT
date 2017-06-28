85f34064891572288ece36e4916b6d1aad2f3f38
PabloBotas <kw375@ix.urz.uni-heidelberg.de>
2017-06-28 00:47:56 -0400

    remove test

--- test/test_buggy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-../adaptSpotEnergies --patient P15_base_plan_buggy \
-                     --cbct adapt_data_P15/week6_cbct.mha \
-                     --vf adapt_data_P15/week6_xform.mha
