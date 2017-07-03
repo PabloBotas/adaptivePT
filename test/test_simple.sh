fd9854c49ae9d1f362f05998262b1ef13b0f92eb
PabloBotas <kw375@ix.urz.uni-heidelberg.de>
2017-07-03 13:45:35 -0400

    New results for raytracing testing on spots

--- test/test_simple.sh
+++ test/test_simple.sh
@@ -4,4 +4,6 @@
                      --cbct adapt_data_P15/week6_cbct.mha \
                      --vf adapt_data_P15/week6_xform.mha \
                      --outdir results \
-                     --output_vf vf.dat
+                     --output_vf vf.dat \
+                     --output_ct_traces trial.dat \
+                     --ct_traces_individual
