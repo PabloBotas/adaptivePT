da63299dc06544d6c6fb194b3aa913f3f8656866
PabloBotas <kw375@ix.urz.uni-heidelberg.de>
2018-03-20 19:25:39 -0400

    Add option for the future
    It might be useless

--- src/extra/opt4D_launcher_template.sh
+++ src/extra/opt4D_launcher_template.sh
@@ -4,7 +4,7 @@ seed=${RANDOM}
 
 echo "Launching Opt4D optimization"
 
-planfile="$1"
+planfile="${1}"
 output="${2}"
 cd ${output}
 opt4D -o ./ \
@@ -20,4 +20,20 @@ opt4D -o ./ \
       --max_time 1200 \
       --write_dose \
       ${planfile} | tee reoptimization.log
+
+# opt4D -o ./ \
+#       --lbfgs --lbfgs_m 20 \
+#       --linesearch_steps 20 \
+#       --dont_project_on_bounds \
+#       --unit_initial_fluence \
+#       --random_seed ${seed} \
+#       --add_step_event "UPDATE_LAGRANGE_MULTIPLIERS,20,20" \
+#       --add_step_event "UPDATE_PENALTY,20,20" \
+#       --constraint_penalty_multiplier 2  \
+#       --stop_fract 0.005 \
+#       --stop_fract_memory 5 \
+#       --max_time 120 \
+#       --write_dose \
+#       ${planfile} | tee reoptimization.log
+
 cd - > /dev/null
